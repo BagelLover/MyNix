0164211e591a2bbea356e7902cc715fff4a1aff8
BagelLover <109992548+BagelLover@users.noreply.github.com>
2025-02-16 21:56:31 -0700

    Update flake.nix
    Added razer-laptop-control for laptop stuff

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    inputs.razerdaemon.url = "github:JosuGZ/razer-laptop-control";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
